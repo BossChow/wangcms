191b48742229099c8644278aa0a471ca1e455da0
kian <zhuce8270@163.com>
2016-11-30 23:24:16 +0800

    init

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'moma',
+    'DB_NAME' => 'wangcms',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' => 'cms_',
+);
